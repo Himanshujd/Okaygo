e40d6d57ed02358b7a95edbf96d3d7d9b6457fdf
Himanshujd <125594997+Himanshujd@users.noreply.github.com>
2023-04-22 09:39:41 +0530

    added driver to open browser

--- login_page.py
+++ login_page.py
@@ -1,2 +1,12 @@
 from selenium import webdriver
+import unittest
 
+class LoginAdminDashboard(unittest.TestCase):
+
+    @classmethod
+    def openBrowser(bro):
+        bro.driver =webdriver.Chrome(executable_path='../driver/chromedriver.exe')
+        bro.driver.implicitly_wait(10)
+        bro.driver.maximize_window()
+
+         
