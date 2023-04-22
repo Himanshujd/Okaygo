b1c369ab015681df29711eb8b47756ebfe798b50
Himanshujd <125594997+Himanshujd@users.noreply.github.com>
2023-04-22 09:51:07 +0530

    added one more line

--- login_page.py
+++ login_page.py
@@ -8,5 +8,7 @@ class LoginAdminDashboard(unittest.TestCase):
         bro.driver =webdriver.Chrome(executable_path='../driver/chromedriver.exe')
         bro.driver.implicitly_wait(10)
         bro.driver.maximize_window()
+        bro.driver.get("https://www.qa-eflex.okaygo.in/")  
+        print("successfully opened")
 
          
