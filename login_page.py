3b98905f4dd3326ed083630387cab120ea2b1e8c
Himanshujd <125594997+Himanshujd@users.noreply.github.com>
2023-04-24 22:12:47 +0530

    added more

--- login_page.py
+++ login_page.py
@@ -1,14 +1,28 @@
 from selenium import webdriver
 import unittest
+import time
+
 
 class LoginAdminDashboard(unittest.TestCase):
+    print("case started")
 
     @classmethod
-    def openBrowser(bro):
-        bro.driver =webdriver.Chrome(executable_path='../driver/chromedriver.exe')
-        bro.driver.implicitly_wait(10)
+    def setupBrowser(bro):
+        bro.driver = webdriver.Chrome(executable_path='..\Driver\chromedriver.exe')
+        bro.driver.implicitly_wait(30)
         bro.driver.maximize_window()
-        bro.driver.get("https://www.qa-eflex.okaygo.in/")  
+
         print("successfully opened")
 
-         
+    def loginCredentials(log):
+        log.driver.get("https://www.qa-eflex.okaygo.in/")
+        log.driver.find_element_by_id("emailId").send_keys("himanshu007")
+        log.driver.find_element_by_id("password").send_keys("Himanshu@123")
+        log.driver.find_element_by_class("Login_loginButton__14J9m").send_keys("Login")
+
+    @classmethod
+    def closeBrowser(bro):
+        bro.time.sleep(3)
+        bro.driver.close()
+
+        print("successfully logedin")
