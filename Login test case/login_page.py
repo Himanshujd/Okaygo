7d18789715315513677a5690c7accc6add0ac832
Himanshujd <125594997+Himanshujd@users.noreply.github.com>
2023-04-27 17:48:16 +0530

    added more test cases for login

--- Login test case/login_page.py
+++ Login test case/login_page.py
@@ -8,7 +8,7 @@ import HtmlTestRunner
 
 
 class LoginAdminDashboard(unittest.TestCase):
-    print("case started")
+    print('case started')
 
     @classmethod
     def setUpClass(cls):
@@ -16,13 +16,44 @@ class LoginAdminDashboard(unittest.TestCase):
         cls.driver.implicitly_wait(10)
         cls.driver.maximize_window()
 
-    def test_login(self):
+        # first test case if user entered the correct user_name and password
+        print('First case- correct username and password')
+
+    def test_login1(self):
         self.driver.get("https://www.qa-eflex.okaygo.in/")
         self.driver_wait = WebDriverWait(self.driver, 10)
         email_input = self.driver_wait.until(EC.visibility_of_element_located((By.ID, "emailId")))
         email_input.send_keys("himanshu007")
         password_input = self.driver_wait.until(EC.visibility_of_element_located((By.ID, "password")))
         password_input.send_keys("Himanshu@123")
+        login_button = self.driver_wait.until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "Login_loginButton__14J9m")))
+        login_button.click()
+
+        # second test case if user entered the Incorrect user_name
+        print('second case- Incorrect username')
+
+    def test_login2(self):
+        self.driver.get("https://www.qa-eflex.okaygo.in/")
+        self.driver_wait = WebDriverWait(self.driver, 10)
+        email_input = self.driver_wait.until(EC.visibility_of_element_located((By.ID, "emailId")))
+        email_input.send_keys("himanshu0")
+        password_input = self.driver_wait.until(EC.visibility_of_element_located((By.ID, "password")))
+        password_input.send_keys("Himanshu@123")
+        login_button = self.driver_wait.until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "Login_loginButton__14J9m")))
+        login_button.click()
+
+        # Third test case if user entered the Incorrect user_name
+        print('Third case- Incorrect password')
+
+    def test_login3(self):
+        self.driver.get("https://www.qa-eflex.okaygo.in/")
+        self.driver_wait = WebDriverWait(self.driver, 10)
+        email_input = self.driver_wait.until(EC.visibility_of_element_located((By.ID, "emailId")))
+        email_input.send_keys("himanshu007")
+        password_input = self.driver_wait.until(EC.visibility_of_element_located((By.ID, "password")))
+        password_input.send_keys("Himanshu@")
         login_button = self.driver_wait.until(
             EC.visibility_of_element_located((By.CLASS_NAME, "Login_loginButton__14J9m")))
         login_button.click()
