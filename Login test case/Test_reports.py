9debe33dc1f7916d10d9d8a2a1f59b56cdac9ca2
Himanshujd <125594997+Himanshujd@users.noreply.github.com>
2023-04-28 13:39:08 +0530

    modified test cases

--- Login test case/Test_reports.py
+++ Login test case/Test_reports.py
@@ -1,14 +1,14 @@
-# import unittest
-# import HtmlTestRunner
-#
-# from login_page import test_login1
-# from login_page import test_login2
-# from login_page import test_login3
-#
-# if __name__ == '__main__':
-#     test_suite = unittest.TestSuite()
-#     test_suite.addTest(unittest.makeSuite(test_login1))
-#     test_suite.addTest(unittest.makeSuite(test_login2))
-#     test_suite.addTest(unittest.makeSuite(test_login3))
-#
-#     unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='C:/Users/himan/PycharmProjects/Okaygo1/reports'))
+import unittest
+import HtmlTestRunner
+
+from login_page import test_login1
+from login_page import test_login2
+from login_page import test_login3
+
+if __name__ == '__main__':
+    test_suite = unittest.TestSuite()
+    test_suite.addTest(unittest.TestLoader().loadTestsFromModule(test_login1))
+    test_suite.addTest(unittest.TestLoader().loadTestsFromModule(test_login2))
+    test_suite.addTest(unittest.TestLoader().loadTestsFromModule(test_login3))
+
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='C:/Users/himan/PycharmProjects/Okaygo1/reports'))
--- Login test case/login_page.py
+++ Login test case/login_page.py
@@ -1,14 +1,15 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-import unittest
 import time
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 import HtmlTestRunner
 
 
 class LoginAdminDashboard(unittest.TestCase):
-    print('case started')
+    print('Automation testing started')
 
     @classmethod
     def setUpClass(cls):
@@ -17,7 +18,6 @@ class LoginAdminDashboard(unittest.TestCase):
         cls.driver.maximize_window()
 
         # first test case if user entered the correct user_name and password
-        print('First case- correct username and password')
 
     def test_login1(self):
         self.driver.get("https://www.qa-eflex.okaygo.in/")
@@ -29,9 +29,9 @@ class LoginAdminDashboard(unittest.TestCase):
         login_button = self.driver_wait.until(
             EC.visibility_of_element_located((By.CLASS_NAME, "Login_loginButton__14J9m")))
         login_button.click()
+        print('First case- correct username and password')
 
         # second test case if user entered the Incorrect user_name
-        print('second case- Incorrect username')
 
     def test_login2(self):
         self.driver.get("https://www.qa-eflex.okaygo.in/")
@@ -43,9 +43,9 @@ class LoginAdminDashboard(unittest.TestCase):
         login_button = self.driver_wait.until(
             EC.visibility_of_element_located((By.CLASS_NAME, "Login_loginButton__14J9m")))
         login_button.click()
+        print('second case- Incorrect username')
 
         # Third test case if user entered the Incorrect user_name
-        print('Third case- Incorrect password')
 
     def test_login3(self):
         self.driver.get("https://www.qa-eflex.okaygo.in/")
@@ -57,7 +57,7 @@ class LoginAdminDashboard(unittest.TestCase):
         login_button = self.driver_wait.until(
             EC.visibility_of_element_located((By.CLASS_NAME, "Login_loginButton__14J9m")))
         login_button.click()
-
+        print('Third case- Incorrect password')
 
     @classmethod
     def tearDownClass(cls):
